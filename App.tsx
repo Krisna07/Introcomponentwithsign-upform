3ff86d7e92690db0233b53bba58dd0a7ba05d804
Krisna07 <68741266+Krisna07@users.noreply.github.com>
2023-03-22 21:43:18 +1100

    2 files modified

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ export default function App() {
     email: false,
     password: false,
   });
+  const [submit, setSubmit] = useState();
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -35,6 +36,19 @@ export default function App() {
     !user.password
       ? setErr((prevState) => ({ ...prevState, password: true }))
       : '';
+    if (user.email && user.password && user.firstName && user.lastName) {
+      setSubmit(
+        `Congratualtion ${
+          user.firstName + ' ' + user.lastName
+        }. You have unlocked 7 days of free trial. `
+      );
+    }
+    setUser({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    });
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -165,6 +179,19 @@ export default function App() {
             <span>Terms and Conditions</span>
           </span>
         </form>
+      </div>
+      <div
+        className="congratulation"
+        style={{ opacity: `${submit ? '1' : '0'}` }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          className="tickIcon"
+        >
+          <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+        </svg>
+        <span className="appendable">{submit ? submit : ''}</span>
       </div>
     </div>
   );
